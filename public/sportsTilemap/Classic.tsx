0c536db93fa339698043607497c5392b996a91f3
KereneL <kerenel@gmail.com>
2024-09-19 13:53:09 +0300

    sports theme

---
+++ public/sportsTilemap/Classic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Classic" tilewidth="8" tileheight="8" tilecount="420" columns="28">
+ <image source="Classic_Graveyard/Tiles/Tileset.png" width="224" height="120"/>
+</tileset>
